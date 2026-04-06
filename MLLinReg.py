7b370cd8d79b70e0ed3d8e7f192fb27b2d17d7b4
Erica Kline <129558975+Erica-KL@users.noreply.github.com>
2026-04-06 13:00:54 -0400

    Add files via upload

--- MLLinReg.py
+++ MLLinReg.py
@@ -1,13 +1,14 @@
+from sklearn.neural_network import MLPRegressor
 import numpy as np
-from sklearn.linear_model import LinearRegression
-
 def trainModel(dataset):
     data = np.array(dataset)
+    X = data[:, :-1]
+    y = data[:, -1]
 
-    X = data[:, 0:5]
-    y = data[:, 5]
-
-    model = LinearRegression()
+    model = MLPRegressor(
+        hidden_layer_sizes=(34, 34),
+        activation='relu',
+        max_iter=2000
+    )
     model.fit(X, y)
-
     return model
